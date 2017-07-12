04d26c865b175fffd4c16273f5f19f39fcdbaa56
Cameryn Boyd <cboyd7@fb.com>
2017-07-12 15:54:16 -0700

    created skeleton for detail view controller and made a push segue from the home cell

--- Snip/DetailViewController.swift
+++ Snip/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  HomeViewController.swift
+//  DetailViewController.swift
 //  Snip
 //
 //  Created by Cameryn Boyd on 7/12/17.
@@ -8,32 +8,22 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class DetailViewController: UIViewController {
     
     // outlets
     @IBOutlet weak var profileImageView: UIImageView!
-    @IBOutlet weak var homeCollectionView: UICollectionView!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var barbershopConstantLabel: UILabel!
+    @IBOutlet weak var barberConstantLabel: UILabel!
+    @IBOutlet weak var priceConstantLabel: UILabel!
     
-    @IBAction func touchCamera(_ sender: Any) {
-    }
     
-    @IBAction func touchSearch(_ sender: Any) {
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCell", for: indexPath) as! HomeCell
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
     }
 
     override func didReceiveMemoryWarning() {
