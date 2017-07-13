5de2fc4db5a5cd115cc79f7c5be3d4c2d788ed2e
Cameryn Boyd <cboyd7@fb.com>
2017-07-13 15:19:11 -0700

    added a refresh function and control

--- Snip/HomeViewController.swift
+++ Snip/HomeViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Parse
+import ParseUI
 
 class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var postArray: [PFObject] = []
     
     // outlets
     @IBOutlet weak var profileImageView: UIImageView!
@@ -22,21 +26,53 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        refresh()
         homeCollectionView.dataSource = self
         homeCollectionView.delegate = self
-
         // Do any additional setup after loading the view.
+        let refreshcontrol = UIRefreshControl()
+        refreshcontrol.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        //add refresh control to the table view
+        homeCollectionView.insertSubview(refreshcontrol, at: 0)
+    }
+    
+    func refresh() {
+        //construct PFQuery
+        let query = PFQuery(className: "Post")
+        query.order(byDescending: "createdAt")
+        query.includeKey("user")
+        query.includeKey("photos")
+        query.includeKey("barber")
+        query.includeKey("profile_pic")
+        query.includeKey("tags")
+        query.limit = 20
+        //fetch data asynchronously
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) in
+            if let posts = posts {
+                self.postArray = posts
+                self.homeCollectionView.reloadData()
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+    //refresh control function
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        refresh()
+        refreshControl.endRefreshing()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCell", for: indexPath) as! HomeCell
-        
+        let posts = self.postArray[indexPath.row]
+        print("here is the post object")
+        cell.post = posts
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 27
+        return postArray.count
     }
 
     override func didReceiveMemoryWarning() {
