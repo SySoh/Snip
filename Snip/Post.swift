fe1c7bbb9ff750ff31055076f36f82247960c9ec
Cameryn Boyd <cboyd7@fb.com>
2017-07-13 14:14:45 -0700

    subclassed post

--- Snip/Post.swift
+++ Snip/Post.swift
@@ -9,26 +9,20 @@
 import Foundation
 import Parse
 
-class Post {
+class Post: PFObject, PFSubclassing {
     var postId: String?
     var user: User?
     var barber: Barber?
     var photos: [PFFile]?
     var tags: [Tag]?
-    var updatedAt: Date?
-    var createdAt: Date?
+//    var updatedAt: Date?
+//    var createdAt: Date?
     var price: Int?
     
-    class func postPost(pictures: UIImage, barber: String, barbershop: String, tags: [Tag], price: Int) {
-        let post = PFObject(className: "Post")
-        post["user"] = PFUser.current
-       post["photos"] = getPFFileFromImage(image: pictures)
-        post["tags"] = tags
-        post["price"] = price
-        post["user"] = PFUser.current()
-        post["barber"] = barber
-        
+    class func parseClassName() -> String {
+        return "Post"
     }
+    
     
     class func getPFFileFromImage(image: UIImage?) -> PFFile? {
         // check if image is not nil
