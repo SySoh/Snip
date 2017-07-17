9a6d9eb77b5e5f5ac8a89af598ab010ac193a4b6
Shao Yie Soh <sysoh@MacBook-Pro-134.dhcp.thefacebook.com>
2017-07-17 14:15:45 -0700

    Working on button delegate functionality

--- Snip/ComposeViewController.swift
+++ Snip/ComposeViewController.swift
@@ -12,7 +12,9 @@ import Parse
 class ComposeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UICollectionViewDataSource  {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    //tagList is used to obtain ALL tags and pass them into the tagView
     var tagList: [PFObject] = []
+    //tagReuse is used to accumulate selected tags. It will be populated by tagView. Probably by a prepareForSegue.
     var tagReuse: [Tag] = []
     
     @IBAction func goBack(_ sender: Any) {
@@ -63,13 +65,14 @@ class ComposeViewController: UIViewController, UIImagePickerControllerDelegate, 
         
     }
     
+        
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagCell", for: indexPath) as! TagCell
-        if (tagReuse.count != 0) {
+        if !(tagReuse.isEmpty) {
             print(tagReuse.count)
             cell.tagName.setTitle(tagReuse[indexPath.item].name, for: .normal)
         } else {
-            cell.tagName.setTitle("Empty Tags", for: .normal)
+            cell.tagName.setTitle("No name", for: .normal)
         }
             return cell
     }
--- Snip/TagsViewController.swift
+++ Snip/TagsViewController.swift
@@ -9,19 +9,21 @@
 import UIKit
 import Parse
 
-class TagsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class TagsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, TagCellDelegate {
     var fullTagList: [PFObject] = []
     var selectedTags: [PFObject] = []
+    var tagList: [String] = []
     
-    @IBOutlet var selectTag: UITapGestureRecognizer!
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet  var collectionView: UICollectionView!
+    
     @IBAction func goBack(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    @IBAction func didTapTag(_ sender: Any) {
-    }
 
+    func addString(tagCell: TagCell) {
+        tagList.append((tagCell.tagName.titleLabel?.text)!)
+        performSegue(withIdentifier: "composeView", sender: tagCell)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
@@ -43,7 +45,7 @@ class TagsViewController: UIViewController, UICollectionViewDelegate, UICollecti
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagCell", for: indexPath) as! TagCell
         if fullTagList.count > 0 {
-            cell.tagName.setTitle(fullTagList[indexPath.item].object(forKey: "name") as! String, for: .normal)
+            cell.tagName.setTitle(fullTagList[indexPath.item].object(forKey: "name") as? String, for: .normal)
         cell.layer.cornerRadius = 50
         cell.clipsToBounds = true
             if cell.isSelected {
@@ -53,24 +55,35 @@ class TagsViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return cell
         
     }
+    func collectionView( collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        if let selectedItems = collectionView.indexPathsForSelectedItems {
+            if selectedItems.contains(indexPath) {
+                collectionView.deselectItem(at: indexPath, animated: true)
+                return false
+            }
+        }
+        return true
+    }
+
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
     
     
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let destVC = segue.destination as! ComposeViewController
+        let source = sender as! UIButton
+        
     }
-    */
+ 
 
 }
