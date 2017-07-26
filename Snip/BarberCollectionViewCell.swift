57bb706bd6f755285fe02bdf86364fb49c7059f1
Shao Yie Soh <sysoh@MacBook-Pro-134.dhcp.thefacebook.com>
2017-07-26 12:57:34 -0700

    Adding list of barbers to barbershop:

---
+++ Snip/BarberCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BarberCollectionViewCell.swift
+//  Snip
+//
+//  Created by Shao Yie Soh on 7/26/17.
+//  Copyright © 2017 Shao Yie Soh. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class BarberCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var barberPic: PFImageView!
+}
