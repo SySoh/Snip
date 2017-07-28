9853e5aff203c17519e04ef1f5757e44db9c001e
Cameryn Boyd <cboyd7@fb.com>
2017-07-28 12:01:46 -0700

    used downloaded font for Snip title font

--- Snip/ExampleNavigationController.swift
+++ Snip/ExampleNavigationController.swift
@@ -15,8 +15,8 @@ class ExampleNavigationController: UINavigationController {
         let appearance = UIBarButtonItem.appearance()
         appearance.setBackButtonTitlePositionAdjustment(UIOffset.init(horizontal: 0.0, vertical: -60), for: .default)
         self.navigationBar.isTranslucent = false
-        self.navigationBar.barTintColor = UIColor.init(red: 250/255.0, green: 250/255.0, blue: 250/255.0, alpha: 0.8)
-        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.init(red: 38/255.0, green: 38/255.0, blue: 38/255.0, alpha: 1.0), NSFontAttributeName: UIFont.systemFont(ofSize: 16.0)]
+        self.navigationBar.barTintColor = UIColor(hex: 0xffffff)
+        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor(hex: 0x1D4159), NSFontAttributeName: UIFont.init(name: "BlessedDay", size: 35.0)]
         self.navigationBar.tintColor = UIColor.init(red: 38/255.0, green: 38/255.0, blue: 38/255.0, alpha: 1.0)
         self.navigationItem.title = "Example"
     }
